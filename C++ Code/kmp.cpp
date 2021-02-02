475585779fffc875edcefc743ab5125084ef4c61
HenryJiang97 <jianghanhua97@hotmail.com>
2021-02-01 22:08:56 -0800

    Updated KMP.cpp

--- C++ Code/kmp.cpp
+++ C++ Code/kmp.cpp
@@ -24,3 +24,42 @@ class Solution {
         return next;
     }
 };
+
+
+
+class Solution {
+public:
+    int strStr(string s, string p) {
+        int ns = s.length(), np = p.length();
+        
+        vector<int> suffix = getSuffix(p);
+        
+        vector<int> dp(ns, 0);
+        dp[0] = s[0] == p[0];
+        
+        for (int i = 1; i < ns; i++) {
+            int j = dp[i - 1];
+            while (j > 0 && s[i] != p[j]) {
+                j = suffix[j - 1];
+            }
+            dp[i] = j + (s[i] == p[j]);
+            if (dp[i] == np)    return i - np + 1;
+        }
+        
+        return -1;
+    }
+    
+private:
+    vector<int> getSuffix(string p) {
+        int np = p.length();
+        vector<int> suffix(np, 0);
+        for (int i = 1; i < np; i++) {
+            int j = suffix[i - 1];
+            while (j > 0 && p[j] != p[i]) {
+                j = suffix[j - 1];
+            }
+            suffix[i] = j + (p[j] == p[i]);
+        }
+        return suffix;
+    }
+};
