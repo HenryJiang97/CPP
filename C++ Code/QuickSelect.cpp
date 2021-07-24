287b6a2b6b1b3fa10fd28572e88ec29a2bd6e1b9
HenryJiang97 <jianghanhua97@hotmail.com>
2021-07-23 18:35:57 -0700

    Updated quickselect

--- C++ Code/QuickSelect.cpp
+++ C++ Code/QuickSelect.cpp
@@ -1,38 +1,25 @@
-// Search for kth smallest element in the array
+// Search for kth largest element in the array
 
 class QuickSelect {
 public:
     int quickSelect(vector<int>& nums, int lo, int hi, int k) {
-        if (lo == hi)    return nums[lo];
-        
-        // Randomly pick one element as a pivot
-        int pivot = lo + rand() % (hi - lo + 1);
-        int j = partition(nums, lo, hi, pivot);
-        
-        if (k == j) {
-            return nums[j];
-        } else if (k < j) {
-            return quickSelect(nums, lo, j - 1, k);
-        } else {
+        int pivot = nums[lo + (hi - lo) / 2];
+        int i = lo, j = hi, p = lo;
+        while (p <= j) {
+            if (nums[p] < pivot) {
+                swap(nums[p++], nums[i++]);
+            } else if (nums[p] > pivot) {
+                swap(nums[p], nums[j--]);
+            } else {
+                p++;
+            }
+        }
+        if (hi - j >= k) {
             return quickSelect(nums, j + 1, hi, k);
+        } else if (hi - (i - 1) >= k) {
+            return pivot;
+        } else {
+            return quickSelect(nums, lo, i - 1, k - (hi - (i - 1)));
         }
-    }
-    
-    // Partition (same as the one in quicksort)
-    int partition(vector<int>& nums, int lo, int hi, int pivot) {
-        int val = nums[pivot];
-        swap(nums[lo], nums[pivot]); 
-        
-        int i = lo + 1, j = hi;
-        while (true) {
-            while (j >= lo + 1 && nums[j] >= val)    j--;
-            while (i <= hi && nums[i] <= val)    i++;
-            if (i >= j)    break;
-            swap(nums[i], nums[j]);
-        }
-
-        swap(nums[lo], nums[j]);
-        
-        return j;
     }
 }
