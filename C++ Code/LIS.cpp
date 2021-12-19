24b62ccb2255907b27d65806c334e90ba520495f
HenryJiang97 <jianghanhua97@hotmail.com>
2021-12-19 11:52:25 +0800

    Updated LIS

--- C++ Code/LIS.cpp
+++ C++ Code/LIS.cpp
@@ -17,4 +17,20 @@ public:
         
         return arr.size();
     }
+
+    int lengthOfLISWithEqual(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> arr;
+        for (int i = 0; i < n; i++) {
+            if (arr.empty() || arr.back() <= nums[i]) {
+                arr.push_back(nums[i]);
+            } else {
+                auto it = upper_bound(arr.begin(), arr.end(), nums[i]);
+                *it = nums[i];
+            }
+        }
+        
+        return arr.size();
+    }
 };
